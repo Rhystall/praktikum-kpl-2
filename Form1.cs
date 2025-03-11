66120a6f6088c536eaf5355159286c3057a9dadc
Rhystalgie <aufamuh12@gmail.com>
2025-03-11 23:56:43 +0700

    tambahan fitur GUI

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace tp2_2211104015
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Halo, " + textBox1.Text + "!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
